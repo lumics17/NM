a718805724530bc323acbe1b6a7031b09c3faea5
lumics17 <147649733+lumics17@users.noreply.github.com>
2023-11-25 09:43:41 +0300

    Update newfile.h

--- Laba 1/newfile.h
+++ Laba 1/newfile.h
@@ -2,11 +2,12 @@
 #include<vector>
 using namespace std;
 void forwardElimination(vector<vector<double>>& augmentedMatrix, int n);
-
 // Обратный ход метода Гаусса
 vector<double> backSubstitution(const vector<vector<double>>& augmentedMatrix, int n);
-
 // Метод Гаусса для решения системы линейных уравнений
 vector<double> gaussElimination(const vector<vector<double>>& A, const vector<double>& B);
 void printAnswer(vector<double> result);
 void printMatrix(const vector<vector<double>>& matrix);
+vector<double> FVector(const vector<double>& X, const vector<double>& B, vector<double>& AX, const vector<vector<double>>& Acopy);
+double FNorm(const vector<double>& F);
+double relativeError(vector<vector<double>>Acopy,vector<double>AX,vector<double>X);
